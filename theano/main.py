18cab8a6a6c286be2453ebef7cd6ce54afb769ad
Nishanth N Koushik <nnk@cse.iitb.ac.in>
2015-10-12 13:10:33 +0530

    generate tests

--- theano/main.py
+++ theano/main.py
@@ -4,7 +4,7 @@ from epsilonGreedy import EpsilonAgent
 
 def runEval():
     agent = EpsilonAgent(10, BasicAgent(20, 10), 0.1)
-    monteCarloEval(agent, 100, 1, 20, 10, "value.txt", "state.txt")
+    monteCarloEval(agent, 50000, 1, 20, 10, "value.txt", "state.txt")
 
 if __name__ == "__main__":
     runEval()
