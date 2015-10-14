8332a5d0b9c94feb0d4c4f679bd905047cded169
Manik <dharmanik500@gmail.com>
2015-10-15 01:29:49 +0530

    changed main

--- theano/main.py
+++ theano/main.py
@@ -3,7 +3,7 @@ from handAgent import BasicAgent
 from epsilonGreedy import EpsilonAgent
 
 def runEval():
-    agent = EpsilonAgent(10, BasicAgent(20, 10), 0.1)
+    agent = EpsilonAgent(10, ValueAgent(20, 10, put_theano_func_here), 0.1)
     monteCarloEval(agent, 50000, 1, 20, 10, "value.txt", "state.txt")
 
 if __name__ == "__main__":
